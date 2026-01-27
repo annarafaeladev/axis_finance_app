978cb341c2098309ad09a0193ab1e8d58616e204
Anna Santana <annasantana.compasso@natura.net>
2026-01-27 18:28:46 -0300

    feat: create fixe_form_page

--- lib/pages/finance_fixed_page.dart
+++ lib/pages/finance_fixed_page.dart
@@ -1,8 +1,9 @@
 import 'package:axis_finance_app/core/di/injector.dart';
+import 'package:axis_finance_app/core/enum/form_action.dart';
 import 'package:axis_finance_app/features/finance/domain/entities/fixa.dart';
 import 'package:axis_finance_app/features/finance/presentation/controllers/finance_fixed_expense_controller.dart';
 import 'package:axis_finance_app/widgets/fixed/fixa_item.dart';
-import 'package:axis_finance_app/widgets/fixed/new_fixed_expense_modal.dart';
+import 'package:axis_finance_app/widgets/fixed/fixe_form_page.dart';
 import 'package:axis_finance_app/widgets/list_item_dynamic.dart';
 import 'package:flutter/material.dart';
 import 'package:axis_finance_app/widgets/content_page_header.dart';
@@ -16,77 +17,78 @@ class FinanceFixedPage extends StatefulWidget {
 }
 
 class _FinanceFixedPage extends State<FinanceFixedPage> {
-  late final FinanceFixedExpenseController _expenseController;
+  late final FinanceFixedExpenseController _fixeExpenseController;
 
   @override
   void initState() {
     super.initState();
-    _expenseController = getIt<FinanceFixedExpenseController>();
-    _expenseController.loadExpenses();
+    _fixeExpenseController = getIt<FinanceFixedExpenseController>();
+    _fixeExpenseController.loadExpenses();
   }
 
-  void _openCreateModal() {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-      ),
-      builder: (_) => NewFixeExpenseModal(
-        onSave:
-            ({
-              required vencimento,
-              required descricao,
-              required valor,
-              required categoria,
-              required pago,
-            }) async {
-              await _expenseController.addFixed(
-                vencimento,
-                descricao,
-                valor,
-                categoria,
-                pago,
-              );
-            },
-      ),
+  Future<void> _openEditPage(Fixa item) async {
+    final FormResult<Fixa>? result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => FixaFormPage(item: item)),
     );
+
+    if (result == null) return;
+
+    switch (result.action) {
+      case FormAction.create:
+        break;
+      case FormAction.update:
+        _fixeExpenseController.updateExpense(
+          result.data!.indexRow,
+          result.data!.vencimento,
+          result.data!.descricao,
+          result.data!.valor,
+          result.data!.categoria,
+          result.data!.pago,
+        );
+      case FormAction.delete:
+        _fixeExpenseController.deleteEntryByIndex(result.index!);
+        break;
+    }
   }
 
-  void _openUpdateModal(Fixa item) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-      ),
-      builder: (_) => NewFixeExpenseModal(
-        entry: item,
-        onSave:
-            ({
-              required vencimento,
-              required descricao,
-              required valor,
-              required categoria,
-              required pago,
-            }) async {
-              await _expenseController.updateExpense(
-                item.indexRow,
-                vencimento,
-                descricao,
-                valor,
-                categoria,
-                pago,
-              );
-            },
-      ),
+  Future<void> _openCreatePage() async {
+    final FormResult<Fixa>? result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const FixaFormPage()),
     );
+
+    if (result == null) return;
+
+    switch (result.action) {
+      case FormAction.create:
+        _fixeExpenseController.addFixed(
+          result.data!.vencimento,
+          result.data!.descricao,
+          result.data!.valor,
+          result.data!.categoria,
+          result.data!.pago,
+        );
+        break;
+      case FormAction.update:
+        _fixeExpenseController.updateExpense(
+          result.data!.indexRow,
+          result.data!.vencimento,
+          result.data!.descricao,
+          result.data!.valor,
+          result.data!.categoria,
+          result.data!.pago,
+        );
+        break;
+      case FormAction.delete:
+         break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _expenseController,
+      animation: _fixeExpenseController,
       builder: (_, __) {
         return SingleChildScrollView(
           padding: const EdgeInsets.all(16),
@@ -98,14 +100,14 @@ class _FinanceFixedPage extends State<FinanceFixedPage> {
                 subtitle: "Gerencie suas despesas",
                 buttonText: "Nova despesa",
                 color: Color(0xFF16A28C),
-                onPressed: _openCreateModal,
+                onPressed: _openCreatePage,
               ),
 
               const SizedBox(height: 20),
 
               FinanceCard(
                 title: "Total fixa",
-                value: _expenseController.totalFixasFormatado,
+                value: _fixeExpenseController.totalFixasFormatado,
                 icon: Icons.radio_button_unchecked,
                 startColor: Color(0xFFDD2B2B),
                 endColor: Color(0xFFE95C38),
@@ -124,14 +126,15 @@ class _FinanceFixedPage extends State<FinanceFixedPage> {
               const SizedBox(height: 16),
               ListItemDynamic<Fixa>(
                 titulo: "Histórico de Saídas",
-                items: _expenseController.fixas,
+                items: _fixeExpenseController.fixas,
                 emptyMessage: "Nenhuma saída registrada",
                 itemBuilder: (context, item) {
                   return FixaItem(
                     item: item,
-                    onEdit: () => _openUpdateModal(item),
-                    onDelete: () =>
-                        _expenseController.deleteEntryByIndex(item.indexRow),
+                    onEdit: () => _openEditPage(item),
+                    onDelete: () => _fixeExpenseController.deleteEntryByIndex(
+                      item.indexRow,
+                    ),
                   );
                 },
               ),
